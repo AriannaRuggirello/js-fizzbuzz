ec2209946325859704f9331855ed65fd508f78f7
Arianna Ruggirello <ruggirelloarianna11@gmail.com>
2023-05-02 17:37:11 +0200

    condition not completed yet

--- js/main.js
+++ js/main.js
@@ -7,11 +7,11 @@ let content = document.getElementById("content")
 console.log(content);
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100
-for(let i = 0; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
 
     //  creare l'elemento quadrato
     let square = document.createElement("span");
-
+    
     // creare classe per quadrato
     square.classList.add("fill");
 
@@ -20,25 +20,33 @@ for(let i = 0; i <= 100; i++){
 
     // aggiungiamo il quadrato dentro il contenitore
     content.append(square);
-
-    
-    if(i % 3 == 0){
-        // multipli di 3 stampi “Fizz” al posto del numero
-        console.log("fizz");
+    // definisco il messaggio da mettere nello square
+    output = ""
+    // la condizione
+    if(i % 3 == 0 && i % 5 == 0){
+          // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
+          output = "FizzBuzz";
+          // console.log("fizzbuzz");
     }
     else if(i % 5 == 0){
         // per i multipli di 5 stampi “Buzz”
-        console.log("buzz");
+        output = "Buzz";
+        // console.log("buzz");
     }
-    else if(i % 3 == 0 && i % 5 == 0){
-        // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”
-        console.log("fizzbuzz");
+    else if(i % 3 == 0){
+       // multipli di 3 stampi “Fizz” al posto del numero
+       output = "Fizz";
+       // console.log("fizz");
     }
     else{
         // se nessuna delle condiioni e verificata stampa il numero
-        console.log(i);
+      output = i;
+        // console.log(i);
     }
+  
+    square.append(output)
 
+    
 }
     
 
